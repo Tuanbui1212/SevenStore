6a3d560c56cf61a20abcf8a50d29c429e743abdc
Bui Anh Tuan <tuanbui.code@gmail.com>
2026-01-17 02:55:54 +0700

    Fix CORS config for frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,11 +15,12 @@ const port = process.env.PORT || 5000; //Sửa như này để config vơi front
 //app.use(cors());
 app.use(
   cors({
-    origin: "*", // Cho phép tất cả domain (test thử trước)
+    origin: "https://seven-store-steel.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
-  })
+  }),
 );
+
 app.use(morgan("combined"));
 
 // Express doc duoc json
